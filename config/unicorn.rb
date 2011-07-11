e062b5dc2614829a8c8a22bf5038e6a22bdb6659
Michael He <hlxwell@gmail.com>
2011-07-11 17:16:09 +0800

    add maintenance page and fix unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,7 +25,7 @@ working_directory "/home/sharedio/app/current" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/unicorn.sock", :backlog => 64
+listen "/home/sharedio/app/shared/tmp/sockets/unicorn.sock", :backlog => 64
 listen 9090, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
